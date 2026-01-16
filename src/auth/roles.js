e4af9cf2d06ee26ce9e79887760989b59dd808d9
Carolina Mendoza <yulibetquiroga@gmail.com>
2026-01-16 01:38:29 -0300

    Limpiar App.jsx y crear estructura profesional de carpetas

---
+++ src/auth/roles.js
@@ -0,0 +1,5 @@
+export const ROLES = {
+  ADMIN: "admin",
+  VENDEDOR: "vendedor",
+  COBRADOR: "cobrador"
+};
---
+++ src/utils/model.js
@@ -0,0 +1,40 @@
+// CLIENTE
+/*
+{
+  nombre,
+  dni,
+  telefono,
+  direccion,
+  ciudad,
+  ubicacion: { lat, lng },
+  fotos: { cliente, dni, tarjeta },
+  fechaAlta
+}
+*/
+
+// PRODUCTO
+/*
+{
+  nombre,
+  categoria,
+  precio,
+  stock,
+  stockMinimo
+}
+*/
+
+// VENTA
+/*
+{
+  clientId,
+  productId,
+  vendedorId,
+  cobradorId,
+  fechaVenta,
+  precioTotal,
+  sena: { monto, metodo },
+  modalidad,
+  cuotasTotales,
+  estado
+}
+*/
